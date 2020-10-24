232743a9a74b4cd5e772a9cfae29c2e8fe2994e6
yjqww6 <343519265@qq.com>
2020-10-24 13:51:29 +0800

    color

---
+++ color.rkt
@@ -0,0 +1,76 @@
+#lang racket
+(require xml syntax-color/module-lexer)
+
+(define (types in)
+  (let loop ([mode #f] [s '()])
+    (define-values (str type _1 start end _4 new-mode)
+      (module-lexer in 0 mode))
+    (cond
+      [(eof-object? str) (reverse s)]
+      [(memq type
+             '(comment sexp-comment constant
+                       string parenthesis hash-colon-keyword symbol))
+       (loop new-mode (cons (vector (sub1 start) (sub1 end) type str) s))]
+      [else (loop new-mode s)])))
+
+(define (colorme str)
+  (define colored
+    (types (open-input-string str)))
+
+  (let loop ([pos 0] [colored colored])
+    (match colored
+      [(cons (vector start end type s) rest)
+       (cond
+         [(< pos start)
+          (cons `(code () ,(substring str pos start))
+                (loop start colored))]
+         [(= pos start)
+          (cons `(code ((class ,(string-append "racket "
+                                               (symbol->string type))))
+                       ,s)
+                (loop end rest))])]
+      [_
+       (cond
+         [(>= pos (string-length str))
+          '()]
+         [else
+          (list `(code () ,(substring str pos)))])])))
+
+(define style
+  #<<style
+code.racket {
+  font-family: 'Fira-Mono', monospace;
+}
+code.parenthesis {
+  color: rgb(132,60,36);
+}
+code.symbol {
+  color: rgb(38, 38, 128);
+}
+code.hash-colon-keyword {
+  color: rgb(38, 38, 128);
+}
+code.string,code.constant {
+  color: rgb(41,128,38);
+}
+code.comment,code.sexp-comment {
+  color: rgb(192,116,31);
+}
+style
+  )
+
+(module+ main
+  (require racket/runtime-path)
+  (define file (path-add-extension (make-temporary-file) ".html"))
+  (define-runtime-path color.rkt "color.rkt")
+  (display-to-file
+   (xexpr->string
+    `(html () (head ()
+                    (style () ,style))
+           (body ()
+                 (pre () ,@(colorme (file->string color.rkt))))))
+   file
+   #:exists 'replace)
+  
+  (require net/sendurl)
+  (send-url/file (path->string file)))
